4e03131630aeb0df2953cced47b42af2a45afd56
Chris Allick <chrisallick@gmail.com>
2013-09-01 11:49:02 -0700

    loop forever random and replace

--- public/js/Instagram.js
+++ public/js/Instagram.js
@@ -28,7 +28,6 @@ Instagram = function( _pics, _el ) {
 				}
 			}
 			img.onerror = function() {
-				//console.log("error");
 				self.loaded++;
 			}
 			img.src = self.pics[i];
@@ -40,7 +39,7 @@ Instagram = function( _pics, _el ) {
 	this.animate = function() {
 		clearTimeout( self.t );
 		var img = getRandomInt( 0, self.images.length-1 );
-		var el = $(".photo:eq(0)");
+		var el = $(".photo:eq("+img+")");
 
 
 		img = getRandomInt( 0, self.images.length-1 );
@@ -54,27 +53,12 @@ Instagram = function( _pics, _el ) {
 		$(el).addClass("slideOut").css({
 			left: $(el).width()+2
 		});
-		
+
 		$(next).animate({
 			left: -1
-		}, 600 )
-
-		// $(nextEl).css("opacity","0");
-
-		// if( imgIndex == replaceIndex ) {
-		// 	self.animate();
-		// } else {
-		// 	$(el).addClass("slideOut").css({
-		// 		left: $(el).width()+10
-		// 	}).bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
-		// 		$(el).after(nextEl);
-		// 		$(el).remove();
-		// 		$(nextEl).animate({
-		// 			opacity: 1
-		// 		}, function() {
-		// 			self.t = setTimeout( self.animate, 2000 );
-		// 		}).removeClass("slideOut");
-		// 	});
-		// }
+		}, 600, function() {
+			$(el).remove();
+			self.t = setTimeout( self.animate, 1000 );
+		})
 	}
 }
