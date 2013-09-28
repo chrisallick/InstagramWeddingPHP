f8cb9cb3134506ee2aa21658e50fbe4ca823865a
Chris Allick <chrisallick@gmail.com>
2013-09-28 08:25:24 -0700

    updated ui

--- public/js/main.js
+++ public/js/main.js
@@ -5,4 +5,18 @@ $(window).load(function(){
 var instagram;
 $(document).ready(function() {
 	instagram = new Instagram( pics, $("#photos") );
+
+	$("#hashtag").css({
+		top: $(document).height()/2 - ($("#hashtag").height()+60)/2,
+		left: $(document).width()/2 - ($("#hashtag").width()+60)/2
+	}).animate({
+		opacity: 1
+	});
+
+	$(window).resize(function(){
+		$("#hashtag").css({
+			top: $(document).height()/2 - ($("#hashtag").height()+60)/2,
+			left: $(document).width()/2 - ($("#hashtag").width()+60)/2
+		});
+	});
 });
