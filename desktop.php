c0a4127b727bbae8f23afd0c0e7a16eac8d90754
Chris Allick <chrisallick@gmail.com>
2013-10-06 14:03:38 -0700

    mobile

---
+++ desktop.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>#craigandbeegetmarried</title>
+
+        <meta charset="utf-8">
+
+        <meta name="viewport" content="width=640, initial-scale=.5, maximum-scale=1">
+        
+        <link href='./css/reset.css' media='all' rel='stylesheet' type='text/css' />
+        <link href='./css/main.css' media='all' rel='stylesheet' type='text/css' />
+        
+        <script src='http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js' type='text/javascript'></script>
+        <script src='./js/jq.easing.js' type='text/javascript'></script>
+        <script src='./js/main.js' type='text/javascript'></script>
+        <script src='./js/Instagram.js' type='text/javascript'></script>
+        <script src='./js/plugins.js' type='text/javascript'></script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="photos" class="clear">
+                
+            </div>
+            <div id="nav">
+                <ul class="clear">
+                    <li><a class="schedule" href="#">schedule</a></li>
+                    <li><a class="registry" href="#">registry</a></li>
+                    <li class="last"><a class="gettinghere" href="#">getting ready</a></li>
+                </ul>
+            </div>
+            <div id="hashtag">
+                <img src="./img/hashtag.png" />
+            </div>
+            <div id="content">
+                <div class="close">x</div>
+                <div id="registry">
+                    <p>Of course no gifts are necessary; we’re thankful you can share our day with us! We live in a small apartment in San Francisco with almost everything we need. We have registered for a <a href="http://www.honeyfund.com/wedding/craigandbee">few things here</a>, but flat gifts in envelopes are also greatly appreciated.</p>
+                </div>
+                <div id="schedule">
+                    <h2>Welcome Gathering</h2>
+                    <p>Thursday 8:30-10:30pm</p>
+                    <p>Please join us at the Alpine Club for some mingling and a brief program about Craig&Bee. Aprés Ski Chic attire. Cash bar available.</p>
+
+                    <br />
+                    <br />
+
+                    <h2>On-Going Snowman Building Contest</h2>
+                    <p>Stop by anytime to add a snowman to our contest gallery! Final judging will occur Sunday morning during the coffee and donuts send-off, with surprise guest judge.  Kids and adults welcome. Location TBD.</p>
+
+                    <br />
+                    <br />
+
+                    <h2>The Big Day</h2>
+                    <p>Friday Morning</p>
+                    <p>Friday Morning</p>
+                    <p>Feel free to ski if you like; just don’t be late! The Village has more than one gondola. Make sure you are using the Highlands Gondola, not Big Springs Express! Reaching the Ritz from the Village by gondola will take about 15 minutes.</p>
+                    <p>4:00-4:30 pm</p>
+                    <p>Follow the signs to the Ritz Carlton Living Room to relax and mingle. Have a drink from the bar or warm yourself by the fire until the ceremony begins.
+                    5:45-midnight</p>
+                    <p>We’ll move indoors for cocktail hour, followed by dinner and tons of dancing. The gondola will run until 10:30am</p>
+
+                    <br />
+                    <br />
+
+                    <h2>A day on the Mountain</h2>
+                    <p>Saturday<br />
+                    <p>This is a day for us all to spend on the mountain. Skiing, tubing, skating.</p>
+                    <p>Planning for this day is still underway. Possibilities include: snow picnic, group tubing, fire and ice party, ice skating.</p>                    
+                    <p>Saturday Evening<br />
+                    <p>If there is a plan, we’ll post it here!</p>
+
+                    <br />
+                    <br />
+
+                    <h2>Coffee and Donuts Snow Picnic Send-Off</h2>
+                    <p>Sunday, 10-12<br />
+                    <p>Before you head out of Tahoe, stop by to witness the grandeur of our Snowman Gallery and the crowning of our contest winner. Plus, coffee and donuts. Location TBD.</p>
+                </div>
+                <div id="gettinghere">
+                    <p>If possible, please arrive for our Welcome Gathering at 8:30 on Thursday night.</p>
+
+                    <p>The wedding itself will be at the Ritz Carlton Hotel. Lodging and other gatherings will be in Northstar Village, which is connected to the Ritz by gondola till 10pm.</p>
+                     
+                    <p>Northstar Resort is 45 minutes from Reno International Airport and a 3-4 hour drive from San Francisco.<br />**Winter driving in this area is intense! Either 4 wheel drive or tire chains are required by law. Please don’t take this lightly!</p>
+
+                    <br />
+                    <br />
+
+                    <h2>Do I Have to Drive?</h2>
+                    <p>Nope. As long as you are staying in Northstar <span class="italics">Village</span>, you can get everywhere on foot or by gondola. For the rest of the resort, free shuttles run until 10 pm. Call one at 530.562.2257. Put it in your phone right now!</p>
+                    
+                    <br />
+                    <br />
+                    
+                    <h2>How Do I Get To And From the Airport?</h2>
+                    <p>Taxis run between Reno Airport and Northstar 24 Hours. One way service is roughly $90 one way for up to 4 people, $10 each additional person.  Try Lakeshore Taxi at 530.544.3600 or Elite Tahoe Taxi at 530.580.8294. Advanced reservations are recommended, especially for groups.</p>
+
+                    <p>A more affordable option, especially for singles, is <a href="http://www.northlaketahoeexpress.com/">North Lake Tahoe Express Shuttle</a>. It’s a scheduled shuttle for $45 one way (or less, if you have more people). Advance reservations are required. Call 866.216.5222. Here are the times:</p>
+                    <p>Leaving Reno Airport daily at:</p>
+                    <table>
+                        <tr>
+                            <td>8:00am</td>
+                            <td>1:00pm</td>
+                            <td>5:30pm</td>
+                            <td>11:15pm</td>
+                        </tr>
+                        <tr>
+                            <td>11:15am</td>
+                            <td>3:15pm</td>
+                            <td>7:30pm</td>
+                            <td></td>
+                        </tr>
+                    </table>
+
+                    <p>Leaving Northstar Village for the airport daily at:</p>
+                    <table> 
+                        <tr>
+                            <td>4:00am</td>
+                            <td>9:15am</td>
+                            <td>5:15pm</td>
+                        </tr>
+                        <tr>
+                            <td>6:00am</td>
+                            <td>1:15pm</td>
+                            <td></td>
+                        </tr>
+                    </table>
+
+                    <br />
+                    <br />
+                     
+                    <h2>Where Do I Sleep n Stuff?</h2>
+                    <p>We have housing of all kinds, and a group rate for most of it. Don't <span class="italics">delay</span>; things will fill up! We would love to have as many guests as possible in the Village so we’re all together.<p>
+
+                    <p>The links below will let you see pics, but <span class="italics">please do not reserve online</span>! The system will not always show you all vacancies or allow you to choose the Village. Read carefully below, then call one of the two numbers</p>
+
+                    <p>Many options have kitchens, multiple bedrooms and baths,  and livings rooms (some with pull-outs). The best way to save is by buddying up!</p>
+
+                    <p>Use <a href="">this map</a> to see the area. Prices are (weeknight/weekend).</p>
+
+                    <p>Finally, the prices you will see on the website do not reflect our discount. The discounted rates are listed for you below (weeknight/weekend).</p>
+
+                    <p class="indent">TAHOE MOUNTAIN RESORTS LODGING: These are really plush, right in the Village with gourmet kitchens, living rooms, fireplaces, full-size washer dryers, underground parking and balconies. Some sleep up to 10. The buildings are: Big Horn, Catamount, Great Bear, Iron Horse and One Village Place (Ignore “Old Greenwood” and “Constellation Residences.”) There is no group rate, but these are still a great deal for 4 or more people; we highly recommend! <a href="http://tinyurl.com/lp2qdlc">See pics here</a> To reserve, call 800.757.7563</p>
+                    <ul>
+                        <li>1 bedroom $349/$479</li>
+                        <li>2 bedroom $479/$689</li>
+                        <li>3 bedroom $759/$899</li>
+                        <li>4 bedrooms may be available; call for pricing</li>
+                    </ul>
+
+                    <p class="indent">NORTHSTAR LODGING: For these reservations call 1.800.GO.NORTH and use group code GB4RMW. You may need to wait for a call back. <a href="http://www.northstarcalifornia.com/info/ski/trip-planning/accommodations-overview.asp#rooms">See pics here</a> but remember the rates here do not reflect our discount. Discounted rates are listed below.</p>
+                    <ul>
+                        <li>Village Rooms: 1 or 2 queen beds, 1 bath, balcony, elevator, daily maid, $196/$217</li>
+                        <li>Village Lofts: 1 bedroom with queen sized beds, 1 bath, living room, fireplace, kitchen, balcony, ski in/out, elevator, daily maid, $273/301</li>
+                        <li>Ski Trails Condos: proper ski in/out 1 and 2 bedrooms, 1 bath, kitchen, fireplace, living room, short walk to the Village, some with loft, ask for “Skyview;” it’s closest to the Village. $231/$252</li>
+                        <li>Aspen Grove studios: queen or queen Murphy bed (ask to be in walking distance of the Village), $175/$196</li>
+                    </ul>
+
+                    <p class="indent">OUTSIDE THE VILLAGE: Once more, we don't recommend staying outside the village. If you are staying in Gold Bend, Indian Hills or lower Aspen Grove, you will need the shuttle. <a href="http://www.northstarcalifornia.com/info/ski/trip-planning/northstar-mountain-condominiums.asp">Pics here</a>; prices below.</p>
+                    <ul>
+                        <li>2 BR, 1 Bath Condos, $294/$315</li>
+                        <li>2 BR, 2 Bath Condos , $315/$343</li>
+                        <li>3 BR, 2 Bath Condos, $406/$427</li>
+                        <li>4 BR, 2 Bath Condos, $476/$511</li>
+                    </ul>
+
+                    <br />
+                    <br />
+
+                    <h2>What about the Ritz?</h2>
+                    <p>Really, you still want to talk about lodging? We do not have a group rate at the Ritz, but you are welcome to stay there and soak your feet in champagne every night. You will be able to reach the Village by gondola until 10 pm.</p>
+
+                    <br />
+                    <br />
+
+                    <h2>Should I Pack Anything Special?</h2>
+                    <p>Yes! Bring snow gear, skis, boards, mittens and a reindeer sleigh. Also a bathing suit for the hot tub. The ceremony will be outside, so please bundle up! The wedding itself is a formal affair, in a suits-not-tuxes kind of way.</p>
+
+                    <br />
+                    <br />
+
+                    <h2>Will we be bombing and shredding?</h2>
+                    <p>Yes! We hope everyone will get out on the mountain as much as possible. We’ll have group rate tickets, but we have to purchase ahead; watch for an email from Bee.</p>
+
+                    <br />
+                    <br />
+
+                    <h2>I don’t slide down mountains on planks of any kind...</h2>
+                    <p>No worries! Visit the schedule tab for our basic itinerary of non-downhill stuff. Also, here are a few suggestions. Some require reservations so plan ahead!</p>
+
+                    <ul>
+                        <li>The Northstar Lodging options include free ice-skating and 2 free skate rentals at the Village Rink. Look for the vouchers in your check-in packet!</li>
+                        <li>Lodging also comes with $2 access to the outdoor spas, pools, fitness center, game room and arcade</li>
+                        <li>Stop by to add your own creative Snowman to our Contest Gallery. Prizes awarded at the Donuts and Coffee Send Off on Sunday morning. Location TBD.</li>
+                        <li>The Ritz Carlton has <a href="http://tiny.cc/g4e53w">an amazing, full service, scenic spa</a>. Schedule ahead for this one!</li>
+                        <li><a href="http://tiny.cc/o2e53w">This link</a> has info for a bunch more activities like snow tubing, pottery painting, candle-making, snowshoeing, cross-country skiing, and using a bungee rig to slingshot yourself up into the winter sky</li>
+                    </ul>
+
+                    <br />
+                    <br />
+
+                    <h2>Are There Child Care Options?</h2>
+                    <p><a href="http://tiny.cc/38e53wOR">North Star Daycare Info</a></p>
+                    <p><a href="http://www.northtahoegroceryservice.com/#!blank/c12t6">North Tahoe Grocery Service On Call Nanny</a></p>
+
+                    <br />
+                    <br />
+
+                    <h2>#craigandbeegetmarried</h2>
+                    <p>We want to see your Craig&Bee-related pics, starting now! Use our #, and your pics will become part of this website too. But only if they are tagged by the original poster, fyi.</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ js/mobile.js
@@ -0,0 +1,66 @@
+var instagram;
+var pics_cache = JSON.parse('["http:\/\/distilleryimage0.s3.amazonaws.com\/1940f1f02e4211e3a4fb22000a1f97ec_6.jpg","http:\/\/distilleryimage7.s3.amazonaws.com\/f48e10822ad011e3967b22000aa80146_6.jpg","http:\/\/distilleryimage6.s3.amazonaws.com\/8209bb562ac611e3920622000ae9085f_6.jpg","http:\/\/distilleryimage0.s3.amazonaws.com\/1e9b12962ac011e3821b22000aeb0baa_6.jpg","http:\/\/distilleryimage9.s3.amazonaws.com\/c2dd3c6a29e111e3ac5222000a1f8ea3_6.jpg","http:\/\/distilleryimage0.s3.amazonaws.com\/007bc95e1cec11e39dc322000a1f9c8f_6.jpg","http:\/\/distilleryimage8.s3.amazonaws.com\/1f832b3a1aa811e3aa5e22000aa80116_6.jpg","http:\/\/distilleryimage5.s3.amazonaws.com\/45bcb6a0075b11e3b78322000a1fac13_6.jpg","http:\/\/distilleryimage11.s3.amazonaws.com\/b0f2095206c511e3b59022000aa80136_6.jpg","http:\/\/distilleryimage5.s3.amazonaws.com\/05eb59c4012911e3a77f22000a9e29a0_6.jpg","http:\/\/distilleryimage4.s3.amazonaws.com\/e31114c2ffeb11e29d7122000a1f97c6_6.jpg","http:\/\/distilleryimage4.s3.amazonaws.com\/a3808abaddc511e286e122000a9e080f_6.jpg","http:\/\/distilleryimage7.s3.amazonaws.com\/4559d28e886611e2b55e22000a9f09fb_6.jpg","http:\/\/distilleryimage4.s3.amazonaws.com\/bcbeaf86881411e2a0d522000a1f970a_6.jpg","http:\/\/distilleryimage6.s3.amazonaws.com\/268cf350881511e2904b22000a1f8c1d_6.jpg","http:\/\/distilleryimage6.s3.amazonaws.com\/ee0c6d587aea11e2a23c22000a1f9d66_6.jpg","http:\/\/distilleryimage2.s3.amazonaws.com\/638e48e8793411e2b7ea22000a1f9366_6.jpg","http:\/\/distilleryimage10.s3.amazonaws.com\/9a328f4e764611e2a73722000a1f9317_6.jpg","http:\/\/distilleryimage10.s3.amazonaws.com\/94c3f19476df11e2b44322000a1f92df_6.jpg","http:\/\/distilleryimage6.s3.amazonaws.com\/f207ff8c6da511e2a61722000a1f9d6d_6.jpg","http:\/\/distilleryimage6.s3.amazonaws.com\/6bf7ed6c68b911e28b3722000a1f99d9_6.jpg","http:\/\/distilleryimage5.s3.amazonaws.com\/17dc6b3268b311e2939b22000a1f9251_6.jpg","http:\/\/distilleryimage9.s3.amazonaws.com\/6752c25868b111e297b922000a1fa527_6.jpg","http:\/\/distilleryimage1.s3.amazonaws.com\/019a6886680111e2a50222000a1fb870_6.jpg","http:\/\/distilleryimage4.s3.amazonaws.com\/ba0d7a9a5cd311e2a52022000a1f9e5e_6.jpg","http:\/\/distilleryimage2.s3.amazonaws.com\/a2d0181e5da111e2bcaf22000a1fbcb3_6.jpg","http:\/\/distilleryimage6.s3.amazonaws.com\/7d47e44e5c8111e2969522000a9f14e8_6.jpg","http:\/\/distilleryimage7.s3.amazonaws.com\/dbc5dc2056f411e2902222000a1fa52b_6.jpg","http:\/\/distilleryimage0.s3.amazonaws.com\/0f950df653cf11e2a7ed22000a1f8f24_6.jpg","http:\/\/distilleryimage10.s3.amazonaws.com\/136696b63c0a11e2a1bd22000a9f1361_6.jpg","http:\/\/distilleryimage7.s3.amazonaws.com\/89e6eb123b6d11e29d0322000a1f97e3_6.jpg","http:\/\/distilleryimage10.s3.amazonaws.com\/6f7620a2365b11e2a9d522000a1fb17d_6.jpg","http:\/\/distilleryimage11.s3.amazonaws.com\/ce5cdfbc34d911e2802422000a9e0927_6.jpg","http:\/\/distilleryimage8.s3.amazonaws.com\/1014dd1831e911e283fd12313817a15a_6.jpg","http:\/\/distilleryimage9.s3.amazonaws.com\/4872fdfa2b7e11e2abce22000a1f96d4_6.jpg","http:\/\/distilleryimage6.s3.amazonaws.com\/8e89d6b6208311e2aaa222000a1fb843_6.jpg","http:\/\/distilleryimage8.s3.amazonaws.com\/5d6ab0221bca11e2984822000a1f9707_6.jpg","http:\/\/distilleryimage8.s3.amazonaws.com\/92e4332e1cc611e3b74e22000a9e07d7_6.jpg","http:\/\/distilleryimage7.s3.amazonaws.com\/be632ae0287f11e393e422000aaa09ed_6.jpg","http:\/\/distilleryimage3.s3.amazonaws.com\/16182c621e3611e3902022000a1ddbd3_6.jpg","http:\/\/distilleryimage2.s3.amazonaws.com\/6a959092fcba11e2aa8c22000a1fc809_6.jpg","http:\/\/distilleryimage1.s3.amazonaws.com\/7865b2a6e76611e2882622000a1f985d_6.jpg","http:\/\/distilleryimage10.s3.amazonaws.com\/c530555ce14211e2ae1422000ae912a5_6.jpg","http:\/\/distilleryimage11.s3.amazonaws.com\/c675d51495ce11e28dc022000a1f8c21_6.jpg","http:\/\/distilleryimage9.s3.amazonaws.com\/ebbf90c8847411e299e022000a1fb043_6.jpg","http:\/\/distilleryimage2.s3.amazonaws.com\/9b4b5dba5c7f11e2a0d822000a1f9a12_6.jpg","http:\/\/distilleryimage11.s3.amazonaws.com\/3ca31c36917c11e180c9123138016265_6.jpg","http:\/\/distilleryimage9.s3.amazonaws.com\/65ff3dc2969311e29ade22000a1f9bd0_6.jpg","http:\/\/distilleryimage9.s3.amazonaws.com\/b137550634e411e2b28822000a9f1468_6.jpg","http:\/\/distilleryimage8.s3.amazonaws.com\/540ac56adcfc11e197621231381b4865_6.jpg","http:\/\/distilleryimage11.s3.amazonaws.com\/2c0ab2c2d34811e195e3123138048d2c_6.jpg","http:\/\/distilleryimage8.s3.amazonaws.com\/e0ec209cced711e1b62322000a1e8a75_6.jpg","http:\/\/distilleryimage4.s3.amazonaws.com\/c4157ae406c711e3983422000ae80f97_6.jpg","http:\/\/distilleryimage11.s3.amazonaws.com\/4ad84caefd5011e2ad6922000a1fa410_6.jpg","http:\/\/distilleryimage5.s3.amazonaws.com\/016e223cfc8311e2bb1e22000a1fc4f4_6.jpg","http:\/\/distilleryimage2.s3.amazonaws.com\/4ede18d8c0fb11e2a52422000aaa0a0f_6.jpg","http:\/\/distilleryimage10.s3.amazonaws.com\/c563164ab85711e2b36e22000a1fa437_6.jpg","http:\/\/distilleryimage3.s3.amazonaws.com\/fe0309e6629011e28a7322000a1fa414_6.jpg"]');
+$(document).ready(function() {
+	/*
+		load from cache
+	*/
+    instagram = new Instagram( pics_cache, $("#photos") );
+	instagram.load(function() {
+		if( !faded ) {
+			$("#hashtag").mousemove(function() {
+				clearTimeout( ft );
+				runFade();
+			});
+		}
+		ft = setTimeout( runFade, 5000 );
+	});
+
+	$("#nav .gettinghere").click(function(event){
+		event.preventDefault();
+		$("#registry").hide().removeClass("open");
+		$("#schedule").hide().removeClass("open");
+		if( $("#gettinghere").hasClass("open") ) {
+			$("#gettinghere").slideUp(function(){
+				$("#content").hide();
+			}).removeClass("open");
+		} else {
+			$("#content").show();
+			$("#gettinghere").slideDown().addClass("open");
+		}
+	});
+
+	$("#nav .schedule").click(function(event){
+		event.preventDefault();
+		$("#registry").hide().removeClass("open");
+		$("#gettinghere").hide().removeClass("open");
+		if( $("#schedule").hasClass("open") ) {
+			$("#schedule").slideUp(function(){
+				$("#content").hide();
+			}).removeClass("open");
+		} else {
+			$("#content").show();
+			$("#schedule").slideDown().addClass("open");
+		}
+	});
+
+	$("#nav .registry").click(function(event){
+		event.preventDefault();
+		$("#gettinghere").hide().removeClass("open");
+		$("#schedule").hide().removeClass("open");
+		if( $("#registry").hasClass("open") ) {
+			$("#registry").slideUp(function(){
+				$("#content").hide();
+			}).removeClass("open");
+		} else {
+			$("#content").show();
+			$("#registry").slideDown().addClass("open");
+		}
+	});
+
+	$(".close").click(function(){
+		$("#gettinghere").hide().removeClass("open");
+		$("#schedule").hide().removeClass("open");
+		$("#registry").hide().removeClass("open");
+		$("#content").hide();
+	});
+});
