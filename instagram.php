ae3089cc9e59281063c5d71d4a4639e79ca7f732
Chris Allick <chrisallick@gmail.com>
2013-10-06 11:20:29 -0700

    fixed include

--- instagram.php
+++ instagram.php
@@ -1,7 +1,9 @@
 <?
+	include "./token.php";
+
 	$pics = array();
 
-	$json = file_get_contents('https://api.instagram.com/v1/tags/craigandbeegetmarried/media/recent?access_token=23858448.b363e5f.c5cb4bc2e0bf430ca6456863abbccab6');
+	$json = file_get_contents('https://api.instagram.com/v1/tags/craigandbeegetmarried/media/recent?access_token=' . $token);
 	$json = json_decode($json, TRUE);
 
 	$next_url = $json["pagination"]["next_url"];
